1b7c6f0cc6771538b1a655be3bd0dd1833043d48
filmList_new <95223859+Nadina00@users.noreply.github.com>
2024-07-21 20:47:38 +0300

    Update RequstError.js

--- helpers/RequstError.js
+++ helpers/RequstError.js
@@ -1,15 +1,16 @@
 const messages = {
     400: 'Bad Request',
     401: 'Unauthorized',
-    403: 'Forbbieden',
-    404: 'Not found',
+    403: 'Forbidden', // Исправлено 'Forbbieden' на 'Forbidden'
+    404: 'Not Found', // Исправлено 'Not found' на 'Not Found'
     409: 'Conflict',
 }
 
-const RequestError = (status, message = messages.status) => {
+const RequestError = (status, message = messages[status]) => {
     const error = new Error(message)
     error.status = status
     
     return error
 }
+
 module.exports = RequestError
